98a00f52aa0b55e9cb9855e646399d0681f072f7
kishan <thekishanabhishek@gmail.com>
2024-04-15 10:55:32 +0530

    database config

--- src/config/database.config.mjs
+++ src/config/database.config.mjs
@@ -1,8 +1,22 @@
+import { configDotenv } from "dotenv";
 import mongoose from "mongoose";
+configDotenv({
+  path: ".development.env",
+});
+const username = encodeURIComponent(process.env.MONGO_USERNAME);
+
+const password = encodeURIComponent(process.env.MONGO_PASSWORD);
 
 export default function databaseConfig() {
   mongoose
-    .connect("mongodb://0.0.0.0:27017/tutorial")
+    .connect(
+      "mongodb+srv://" +
+        username +
+        ":" +
+        password +
+        "@freecodecamp.l0wki4o.mongodb.net/?retryWrites=true&w=majority&appName=freecodecamp",
+      { dbName: "users" }
+    )
     .then(() => console.log("Database Connected"))
     .catch((err) => {
       console.log("🚀 ~ databaseConfig ~ err:", err.message);
